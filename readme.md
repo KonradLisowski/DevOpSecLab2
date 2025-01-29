064e17b752af5784f38a50e534f39376c58529b9
KonradLisowski <vkonradlisowskiv@outlook.com>
2025-01-29 22:44:09 +0000

    Added readme file and updated script

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Week 2 Lab – Bash & Git
+Konrad Lisowski 21376396
+[DevOpSecLab2](https://github.com/KonradLisowski/DevOpSecLab2)
+
+---
+To run the script.sh run: `bash script.sh`
--- script.sh
+++ script.sh
@@ -3,3 +3,8 @@ rm -rf fol_1 fol_2
 mkdir fol_1 fol_2
 touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+
+chmod 700 fol_1/1_1.txt fol_1/1_3.txt fol_2/2_1.txt fol_2/2_3.txt
+chmod 777 fol_1/1_2.txt fol_2/2_2.txt
+
+echo "Job completed"
